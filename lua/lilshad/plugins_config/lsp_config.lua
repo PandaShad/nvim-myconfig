4cf2ee4dffdcd325bef830e188e06cb6a8bb62fe
Maxime Bellet <maximebellet83@hotmail.fr>
2023-08-14 11:21:16 +0200

    lsp zero

--- lua/lilshad/plugins_config/lsp_config.lua
+++ lua/lilshad/plugins_config/lsp_config.lua
@@ -1,4 +1,11 @@
-local lsp = require('lsp-zero').preset({})
+local lsp = require('lsp-zero')
+
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+  'tsserver',
+  'rust_analyzer',
+})
 
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
